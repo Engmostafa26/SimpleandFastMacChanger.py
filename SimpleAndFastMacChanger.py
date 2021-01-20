6a241d675f166bf3a35435c1901ec1af76017e05
Engmostafa26 <53053339+Engmostafa26@users.noreply.github.com>
2021-01-20 14:31:09 -0500

    Update SimpleAndFastMacChanger.py

--- SimpleAndFastMacChanger.py
+++ SimpleAndFastMacChanger.py
@@ -4,5 +4,5 @@ interface = "eth0"
 new_mac = "00:11:22:33:44:55"
 print("--> changing the mac of "+interface+" to "+new_mac)
 subprocess.call("ifconfig "+interface+" down",shell=True)
-subprocess.call("ifconfig "+interface+" hw ether"+new_mac,shell=True)
+subprocess.call("ifconfig "+interface+" hw ether "+new_mac,shell=True)
 subprocess.call("ifconfig "+interface+" up",shell=True)
