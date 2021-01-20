d0f4695d2ab50ae116db92141ad77e4a4d1f4229
Engmostafa26 <53053339+Engmostafa26@users.noreply.github.com>
2021-01-20 14:27:07 -0500

    Update SimpleAndFastMacChanger.py

--- SimpleAndFastMacChanger.py
+++ SimpleAndFastMacChanger.py
@@ -2,7 +2,7 @@
 import subprocess
 interface = "eth0"
 new_mac = "00:11:22:33:44:55"
-print("--> chaning the mac of "+interface+" to "+new_mac)
-subprocess.call("ifconfig eth0 down",shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55",shell=True)
-subprocess.call("ifconfig eth0 up",shell=True)
+print("--> changing the mac of "+interface+" to "+new_mac)
+subprocess.call("ifconfig "+interface+" down",shell=True)
+subprocess.call("ifconfig "+interface+" hw ether"+new_mac,shell=True)
+subprocess.call("ifconfig "+interface+" up",shell=True)
